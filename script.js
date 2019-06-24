0728545f20c46ce6f185a9a92e3c211a6dc72479
splinterific <markgpreston@gmail.com>
2019-06-24 23:02:10 +0100

    updated social media buttons and colours

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ $(".nav_btn").on("click", function () {
     $('.menu').toggleClass("show");
 });
 
-
 function scrollWin(id) {
-    // document.location.hash = "#main-body";
     document.getElementById(id).scrollIntoView(true);
 }
